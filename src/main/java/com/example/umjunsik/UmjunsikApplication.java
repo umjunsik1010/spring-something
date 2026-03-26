f73c79c1c0aafe1fa7369712631a2296612fd103
umjunsik1010 <s20989120@gmail.com>
2026-03-26 19:05:03 +0900

    add all files

---
+++ src/main/java/com/example/umjunsik/UmjunsikApplication.java
@@ -0,0 +1,13 @@
+package com.example.umjunsik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmjunsikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmjunsikApplication.class, args);
+	}
+
+}
